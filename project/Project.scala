e021b99e6cf8cd82241b65f0c405532dc1a1f3e2
Manuel Laflamme <manuel@waj.am>
2013-09-24 10:15:52 -0400

    Tidy up .gitignore file & udate scalatest version

--- project/Project.scala
+++ project/Project.scala
@@ -23,7 +23,7 @@ object BwlBuild extends Build {
     "com.wajam" %% "nrv-core" % "0.1-SNAPSHOT",
     "com.wajam" %% "spnl-core" % "0.1-SNAPSHOT",
     "com.wajam" %% "scn-core" % "0.1-SNAPSHOT",
-    "org.scalatest" %% "scalatest" % "1.7.1" % "test,it",
+    "org.scalatest" %% "scalatest" % "1.9.1" % "test,it",
     "junit" % "junit" % "4.10" % "test,it",
     "org.mockito" % "mockito-core" % "1.9.0" % "test,it"
   )
