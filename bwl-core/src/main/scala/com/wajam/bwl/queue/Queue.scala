cc0bb0cee47470b43814bd30d4130aa933d73638
Manuel Laflamme <manuel@waj.am>
2013-09-25 15:00:08 -0400

    Simple service implementation with in-memory queue

---
+++ bwl-core/src/main/scala/com/wajam/bwl/queue/Queue.scala
@@ -0,0 +1,36 @@
+package com.wajam.bwl.queue
+
+import com.wajam.spnl.feeder.Feeder
+import com.wajam.nrv.data.Message
+import com.wajam.nrv.utils.timestamp.Timestamp
+import com.wajam.bwl.utils.WeightedItemsSelector
+
+case class Priority(value: Int, weight: Int)
+
+class PrioritySelector(priorities: Seq[Priority])
+  extends WeightedItemsSelector(priorities.map(p => (p.weight.toDouble, p.value)))
+
+case class QueueDefinition(name: String, priorities: Seq[Priority] = Seq(Priority(1, weight = 1)))
+
+trait QueueFactory {
+  def apply(token: Long, definition: QueueDefinition): Queue
+}
+
+trait Queue {
+
+  def token: Long
+
+  def name: String
+
+  def priorities: Seq[Priority]
+
+  def enqueue(task: Message, priority: Int)
+
+  def ack(id: Timestamp)
+
+  def feeder: Feeder
+
+  def start()
+
+  def stop()
+}
